8c3a4b8196215371b68ea645a1f40505aae4d6ed
Charles-Henri Decultot <chdecultot@dokos.io>
2023-11-28 18:16:14 +0100

    fix: Number parsing and totals

--- ocr/ocr/doctype/ocr_request/ocr_request.py
+++ ocr/ocr/doctype/ocr_request/ocr_request.py
@@ -141,7 +141,7 @@ class OCRRequest(Document):
 					row = {}
 					for row_key, row_value in child.items():
 						if row_key in ["QUANTITY", "UNIT_PRICE", "PRICE"]:
-							row[row_key.lower()] = self.get_first_floats(row_value)
+							row[row_key.lower()] = parse_number(row_value)
 						else:
 							row[row_key.lower()] = row_value
 					self.append("line_items_mapping", row)
@@ -152,10 +152,10 @@ class OCRRequest(Document):
 				})
 
 			if key == GRAND_TOTAL_KEY:
-				self.grand_total = self.get_first_floats(value)
+				self.grand_total = parse_number(value)
 
 			if key == TAX_TOTAL_KEY:
-				self.tax_total = self.get_first_floats(value)
+				self.tax_total = parse_number(value)
 
 	def get_header_dict(self):
 		return {line.key: line.value for line in self.header_mapping}
@@ -194,19 +194,6 @@ class OCRRequest(Document):
 		for line in self.line_items_mapping:
 			if "supplier" in header and header.get("supplier"):
 				line.item_code = self.get_supplier_item(header["supplier"], line.get("item"))
-
-	def get_first_floats(self, data):
-		data = data.replace(" ", "")
-		if data and (matching_floats := FLOAT_PATTERN.findall(data)):
-			for matching_float in matching_floats[0]:
-				try:
-					if matching_float:
-						value = matching_float.replace(" ", "").replace(",", ".") # Temporary hack to parse floats in french invoices. To be enhanced with different number formats.
-						return float(value) 
-				except Exception:
-					continue
-
-		return data
 
 	def get_supplier_name(self):
 		header = self.get_header_dict()
@@ -336,3 +323,129 @@ def register_supplier_mapping(validated_data, service="AWS Textract"):
 					})
 					item.flags.ignore_permissions = True
 					item.save()
+
+
+
+def parse_number(text):
+	# Borrowed from https://github.com/hayj/SystemTools/blob/master/systemtools/number.py
+	"""
+		Return the first number in the given text for any locale.
+		TODO we actually don't take into account spaces for only
+		3-digited numbers (like "1 000") so, for now, "1 0" is 10.
+		TODO parse cases like "125,000.1,0.2" (125000.1).
+
+		:example:
+		>>> parseNumber("a 125,00 €")
+		125
+		>>> parseNumber("100.000,000")
+		100000
+		>>> parseNumber("100 000,000")
+		100000
+		>>> parseNumber("100,000,000")
+		100000000
+		>>> parseNumber("100 000 000")
+		100000000
+		>>> parseNumber("100.001 001")
+		100.001
+		>>> parseNumber("$.3")
+		0.3
+		>>> parseNumber(".003")
+		0.003
+		>>> parseNumber(".003 55")
+		0.003
+		>>> parseNumber("3 005")
+		3005
+		>>> parseNumber("1.190,00 €")
+		1190
+		>>> parseNumber("1190,00 €")
+		1190
+		>>> parseNumber("1,190.00 €")
+		1190
+		>>> parseNumber("$1190.00")
+		1190
+		>>> parseNumber("$1 190.99")
+		1190.99
+		>>> parseNumber("$-1 190.99")
+		-1190.99
+		>>> parseNumber("1 000 000.3")
+		1000000.3
+		>>> parseNumber('-151.744122')
+		-151.744122
+		>>> parseNumber('-1')
+		-1
+		>>> parseNumber("1 0002,1.2")
+		10002.1
+		>>> parseNumber("")
+
+		>>> parseNumber(None)
+
+		>>> parseNumber(1)
+		1
+		>>> parseNumber(1.1)
+		1.1
+		>>> parseNumber("rrr1,.2o")
+		1
+		>>> parseNumber("rrr1rrr")
+		1
+		>>> parseNumber("rrr ,.o")
+
+	"""
+	try:
+		# First we return None if we don't have something in the text:
+		if text is None:
+			return None
+		if isinstance(text, int) or isinstance(text, float):
+			return text
+		text = text.strip()
+		if text == "":
+			return None
+		# Next we get the first "[0-9,. ]+":
+		n = re.search("-?[0-9]*([,. ]?[0-9]+)+", text).group(0)
+		n = n.strip()
+		if not re.match(".*[0-9]+.*", text):
+			return None
+		# Then we cut to keep only 2 symbols:
+		while " " in n and "," in n and "." in n:
+			index = max(n.rfind(','), n.rfind(' '), n.rfind('.'))
+			n = n[0:index]
+		n = n.strip()
+		# We count the number of symbols:
+		symbolsCount = 0
+		for current in [" ", ",", "."]:
+			if current in n:
+				symbolsCount += 1
+		# If we don't have any symbol, we do nothing:
+		if symbolsCount == 0:
+			pass
+		# With one symbol:
+		elif symbolsCount == 1:
+			# If this is a space, we just remove all:
+			if " " in n:
+				n = n.replace(" ", "")
+			# Else we set it as a "." if one occurence, or remove it:
+			else:
+				theSymbol = "," if "," in n else "."
+				if n.count(theSymbol) > 1:
+					n = n.replace(theSymbol, "")
+				else:
+					n = n.replace(theSymbol, ".")
+		else:
+			# Now replace symbols so the right symbol is "." and all left are "":
+			rightSymbolIndex = max(n.rfind(','), n.rfind(' '), n.rfind('.'))
+			rightSymbol = n[rightSymbolIndex:rightSymbolIndex+1]
+			if rightSymbol == " ":
+				return parseNumber(n.replace(" ", "_"))
+			n = n.replace(rightSymbol, "R")
+			leftSymbolIndex = max(n.rfind(','), n.rfind(' '), n.rfind('.'))
+			leftSymbol = n[leftSymbolIndex:leftSymbolIndex+1]
+			n = n.replace(leftSymbol, "L")
+			n = n.replace("L", "")
+			n = n.replace("R", ".")
+		# And we cast the text to float or int:
+		n = float(n)
+		if n.is_integer():
+			return int(n)
+		else:
+			return n
+	except: pass
+	return None
