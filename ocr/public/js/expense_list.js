1816d899d82547210234c3526636f57b78b47a88
Charles-Henri Decultot <chdecultot@dokos.io>
2024-01-25 15:24:57 +0100

    fix: Remove unfinished feature

--- ocr/public/js/expense_list.js
+++ ocr/public/js/expense_list.js
@@ -1,7 +1,7 @@
 frappe.listview_settings["Expense"] = {
 	onload: function (doclist) {
-		doclist.page.add_action_item(__("Add Receipts"), () => {
-			console.log("Hello")
-		}, false);
+		// doclist.page.add_action_item(__("Add Receipts"), () => {
+		// 	TODO: Allow synchronous OCR for expenses
+		// }, false);
 	}
 }
