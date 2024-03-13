81102691ee5421265a1eccee6a441e84066dd9cf
Charles-Henri Decultot <chdecultot@dokos.io>
2024-03-13 11:32:22 +0100

    fix: Continue processing if request creation has been launched

--- ocr/ocr/doctype/ocr_purchase_invoice_basket/ocr_purchase_invoice_basket.py
+++ ocr/ocr/doctype/ocr_purchase_invoice_basket/ocr_purchase_invoice_basket.py
@@ -94,6 +94,7 @@ def check_requests_completion():
 				if doc.status != "Not Started":
 					doc.db_set("status", "Not Started")
 					doc.run_method("create_requests")
+					continue
 			except Exception:
 				doc.log_error()
 
