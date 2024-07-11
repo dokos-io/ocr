115d8c5a8122c0b30fc1583fecd3f6b774e0e6ef
Charles-Henri Decultot <chdecultot@dokos.io>
2024-07-11 15:33:41 +0200

    fix: order to invoice workflow fixes

--- ocr/public/js/purchase_order.js
+++ ocr/public/js/purchase_order.js
@@ -4,4 +4,17 @@ frappe.ui.form.on('Purchase Order', {
 	refresh(frm) {
 		ocr.original_file_preview(frm);
 	},
+
+	after_save: function (frm) {
+		if (frm.doc.docstatus == 1 ) {
+			frappe.run_serially([
+				() => frappe.timeout(1),
+				() => {
+					if (frm.doc.ocr_request) {
+						frappe.set_route("Form", "OCR Request", frm.doc.ocr_request);
+					}
+				},
+			]);
+		}
+	},
 })
