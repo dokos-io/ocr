cd9294313c9f64de5fbf3d43b96e9f65c341f3da
Corentin Forler <8860073-cforler_dokos@users.noreply.gitlab.com>
2024-07-22 15:21:57 +0200

    fix(overrides): Handle doc=None in get_item_details

--- ocr/overrides.py
+++ ocr/overrides.py
@@ -4,13 +4,11 @@ from erpnext.stock.get_item_details import get_item_details as _get_item_details
 
 @frappe.whitelist()
 def get_item_details(args, doc=None, for_validate=False, overwrite_warehouse=True):
-	"""
-	This function doesn't allow 
-	"""
-
-	doc_values = frappe.parse_json(args)
 	item_details = _get_item_details(args, doc, for_validate, overwrite_warehouse)
-	if frappe.parse_json(doc).get("ocr_request"):
-		item_details["price_list_rate"] = doc_values.get("net_rate")
+
+	if (frappe.parse_json(doc) or {}).get("ocr_request"):
+		doc_values = frappe.parse_json(args) or {}
+		if doc_values:
+			item_details["price_list_rate"] = doc_values.get("net_rate")
 
 	return item_details
