fc147a0e368968f238eef3aa65030e2108c3a40d
Charles-Henri Decultot <chdecultot@dokos.io>
2024-08-08 11:17:40 +0200

    fix: improve condition for redirection after so save

--- ocr/public/js/purchase_order.js
+++ ocr/public/js/purchase_order.js
@@ -6,15 +6,17 @@ frappe.ui.form.on('Purchase Order', {
 	},
 
 	after_save: function (frm) {
-		if (frm.doc.docstatus == 1 ) {
-			frappe.run_serially([
-				() => frappe.timeout(1),
-				() => {
-					if (frm.doc.ocr_request) {
-						frappe.set_route("Form", "OCR Request", frm.doc.ocr_request);
-					}
-				},
-			]);
-		}
+		frappe.db.get_single_value("OCR Settings", "do_not_create_purchase_invoices").then(r => {
+			if (!r.do_not_create_purchase_invoices && frm.doc.docstatus == 1 && frm.doc.per_billed < 100.0) {
+				frappe.run_serially([
+					() => frappe.timeout(1),
+					() => {
+						if (frm.doc.ocr_request) {
+							frappe.set_route("Form", "OCR Request", frm.doc.ocr_request);
+						}
+					},
+				]);
+			}
+		})
 	},
 })
