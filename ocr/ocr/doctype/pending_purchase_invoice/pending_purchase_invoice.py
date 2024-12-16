2681a364055012fd6b320e9d2098ee7c12d36ec5
Charles-Henri Decultot <chdecultot@dokos.io>
2024-12-16 22:33:14 +0100

    refactor: OCR extraction

--- ocr/ocr/doctype/pending_purchase_invoice/pending_purchase_invoice.py
+++ ocr/ocr/doctype/pending_purchase_invoice/pending_purchase_invoice.py
@@ -5,8 +5,9 @@ from frappe.model.meta import data_fieldtypes, default_fields
 
 from frappe.query_builder import Order
 from frappe import _
-from frappe.utils import flt
+from frappe.utils import flt, nowdate
 from erpnext.controllers.accounts_controller import AccountsController
+from erpnext.accounts.party import get_due_date
 
 import frappe
 from frappe.utils import sbool
@@ -34,6 +35,7 @@ class PendingPurchaseInvoice(AccountsController):
 		grand_total: DF.Currency
 		items: DF.Table[PendingPurchaseInvoiceItem]
 		net_total: DF.Currency
+		ocr_request: DF.Link | None
 		posting_date: DF.Date
 		status: DF.Literal["Pending", "In Progress", "Completed"]
 		supplier: DF.Link
@@ -47,8 +49,22 @@ class PendingPurchaseInvoice(AccountsController):
 	# end: auto-generated types
 
 	def validate(self):
-		self.title = f"{self.supplier} : {self.bill_no}"[:140]
+		self.title = f"{self.supplier}"[:140] if self.bill_no else f"{self.supplier} : {self.bill_no}"[:140]
+		self.calculate_due_date()
 		self.calculate_totals()
+
+	def calculate_due_date(self):
+		if not self.due_date and self.supplier:
+			try:
+				self.due_date = get_due_date(
+					posting_date=self.bill_date or nowdate(),
+					party_type="Supplier",
+					party=self.supplier,
+					company=self.company,
+					bill_date=self.bill_date
+				)
+			except Exception:
+				pass
 
 	def set_pending_purchase_order_status(self, commit=False):
 		status = "Pending"
@@ -180,6 +196,103 @@ class PendingPurchaseInvoice(AccountsController):
 			doc.submit()
 
 		return doc
+
+	# def get_matched_orders(self):
+	# 	purchase_order_dt = frappe.qb.DocType("Purchase Order")
+	# 	purchase_invoice_item_dt = frappe.qb.DocType("Purchase Invoice Item")
+
+	# 	subquery = (
+	# 		frappe.qb.from_(purchase_invoice_item_dt)
+	# 		.select(purchase_invoice_item_dt.name)
+	# 		.where(purchase_invoice_item_dt.docstatus.lt(2))
+	# 		.where(purchase_invoice_item_dt.purchase_order == purchase_order_dt.name)
+	# 	)
+
+	# 	query = (
+	# 		frappe.qb.from_(purchase_order_dt)
+	# 		.select(purchase_order_dt.name, purchase_order_dt.net_total, purchase_order_dt.order_confirmation_no, purchase_order_dt.company)
+	# 		.where((purchase_order_dt.docstatus == 1) & (purchase_order_dt.per_billed.lt(100)) & (purchase_order_dt.status.notin(["Closed", "Completed"])))
+	# 		.where(ExistsCriterion(subquery).negate())
+	# 	)
+
+	# 	if self.supplier:
+	# 		query = query.where(purchase_order_dt.supplier == self.supplier)
+
+	# 	open_orders = query.run(as_dict=True)
+
+	# 	matched_orders = set()
+	# 	if self.get_creation_mode() == "Get items from purchase orders recognized by the OCR":
+	# 		for child in self.line_items_mapping:
+	# 			for open_order in open_orders:
+	# 				if re.search(r"(?<![\w\d])" + re.escape(open_order.name) + r"(?![\w\d])", child.get("expense_row") or "", re.IGNORECASE):
+	# 					matched_orders.add(open_order.name)
+	# 				elif re.search(r"(?<![\w\d])" + re.escape(open_order.order_confirmation_no or "") + r"(?![\w\d])", child.get("expense_row") or "", re.IGNORECASE):
+	# 					matched_orders.add(open_order.name)
+	# 	elif open_orders:
+	# 		if closest_order := min(open_orders, key=lambda x:abs(x.net_total - self.net_total)):
+	# 			matched_orders.add(closest_order.name)
+
+	# 	return matched_orders
+
+	# def find_line_items_correspondence(self):
+	# 	for line in self.line_items_mapping:
+	# 		if line.item_code:
+	# 			continue
+
+	# 		line.item_code = self.get_supplier_item(line.get("item")) or self.get_previous_correspondance(line.get("item"))
+
+	# def get_purchase_invoice_qty(self, row):
+	# 	if row.get("unit_price") and row.get("price") and row.get("price") != row.get("unit_price"):
+	# 		return flt(row.get("price") ) / flt(row.get("unit_price"))
+
+	# 	elif row.get("quantity"):
+	# 		return row.get("quantity")
+
+	# 	return 1
+
+	# def get_purchase_invoice_uom(self, row):
+	# 	expense_row = row.get("EXPENSE_ROW")
+
+	# 	if expense_row and (before_unit_price := re.search(r".+?(?=" + re.escape(row.get("unit_price")) + r")", expense_row, re.IGNORECASE)):
+	# 		for section in reversed(before_unit_price[0].strip().split()):
+	# 			if not section.isnumeric() and frappe.db.get_value("UOM", section):
+	# 				return section
+
+	# 	return frappe.db.get_single_value("Stock Settings", "stock_uom")
+
+	# def get_supplier_item(self, item):
+	# 	if not self.supplier:
+	# 		return
+
+	# 	return frappe.db.get_value(
+	# 		"Item Supplier",
+	# 		dict(
+	# 			supplier=self.supplier,
+	# 			supplier_part_no=item
+	# 		),
+	# 		"parent"
+	# 	)
+
+	# def get_previous_correspondance(self, item):
+	# 	if not self.company or not self.supplier:
+	# 		return
+
+	# 	ocr_request = frappe.qb.DocType("OCR Request")
+	# 	ocr_request_line_mapping = frappe.qb.DocType("OCR Line Items Mapping")
+
+	# 	query = (
+	# 		frappe.qb.from_(ocr_request)
+	# 		.left_join(ocr_request_line_mapping)
+	# 		.on(ocr_request.name == ocr_request_line_mapping.parent)
+	# 		.select(ocr_request_line_mapping.item_code, ocr_request.name)
+	# 		.where(ocr_request.company == self.company)
+	# 		.where(ocr_request.supplier == self.supplier)
+	# 		.where(ocr_request_line_mapping.item == item)
+	# 		.where(ocr_request_line_mapping.item_code.isnotnull())
+	# 		.orderby(ocr_request.modified, order=Order.desc)
+	# 	)
+	# 	if result := query.run(as_dict=True):
+	# 		return result[0].item_code
 
 
 def make_purchase_order(source_name, target_doc=None, ignore_permissions=False, simulation=False):
