d51bbb95c30c27a6869613155552e7d536ba3128
Charles-Henri Decultot <chdecultot@dokos.io>
2024-12-18 18:54:09 +0100

    fix: add a try catch for image preview

--- ocr/ocr/doctype/pending_purchase_invoice/pending_purchase_invoice.js
+++ ocr/ocr/doctype/pending_purchase_invoice/pending_purchase_invoice.js
@@ -68,46 +68,51 @@ frappe.ui.form.on("Pending Purchase Invoice", {
 	async show_preview(frm) {
 		let $preview = "";
 		await frappe.model.with_doc("File", frm.doc.file);
-		const file_doc = frappe.model.get_doc("File", frm.doc.file);
-		let file_extension = file_doc.file_type.toLowerCase();
-		const file_preview_field = frm.get_field("invoice_preview");
+		try {
+			const file_doc = frappe.model.get_doc("File", frm.doc.file);
+			let file_extension = file_doc.file_type.toLowerCase();
+			const file_preview_field = frm.get_field("invoice_preview");
 
-		if (frappe.utils.is_image_file(file_doc.file_url)) {
-			$preview = $(`<div class="img_preview">
-				<img
-					class="img-responsive"
-					src="${frappe.utils.escape_html(file_doc.file_url)}"
-				/>
-			</div>`);
-		} else if (frappe.utils.is_video_file(file_doc.file_url)) {
-			$preview = $(`<div class="img_preview">
-				<video width="480" height="320" controls>
-					<source src="${frappe.utils.escape_html(file_doc.file_url)}">
-					${__("Your browser does not support the video element.")}
-				</video>
-			</div>`);
-		} else if (file_extension === "pdf") {
-			$preview = $(`<div class="img_preview">
-				<object style="background:#323639;" width="100%">
-					<embed
-						style="background:#323639;"
-						width="100%"
-						height="1190"
-						src="${frappe.utils.escape_html(file_doc.file_url)}" type="application/pdf"
-					>
-				</object>
-			</div>`);
-		} else if (file_extension === "mp3") {
-			$preview = $(`<div class="img_preview">
-				<audio width="480" height="60" controls>
-					<source src="${frappe.utils.escape_html(file_doc.file_url)}" type="audio/mpeg">
-					${__("Your browser does not support the audio element.")}
-				</audio >
-			</div>`);
-		}
+			if (frappe.utils.is_image_file(file_doc.file_url)) {
+				$preview = $(`<div class="img_preview">
+					<img
+						class="img-responsive"
+						src="${frappe.utils.escape_html(file_doc.file_url)}"
+					/>
+				</div>`);
+			} else if (frappe.utils.is_video_file(file_doc.file_url)) {
+				$preview = $(`<div class="img_preview">
+					<video width="480" height="320" controls>
+						<source src="${frappe.utils.escape_html(file_doc.file_url)}">
+						${__("Your browser does not support the video element.")}
+					</video>
+				</div>`);
+			} else if (file_extension === "pdf") {
+				$preview = $(`<div class="img_preview">
+					<object style="background:#323639;" width="100%">
+						<embed
+							style="background:#323639;"
+							width="100%"
+							height="1190"
+							src="${frappe.utils.escape_html(file_doc.file_url)}" type="application/pdf"
+						>
+					</object>
+				</div>`);
+			} else if (file_extension === "mp3") {
+				$preview = $(`<div class="img_preview">
+					<audio width="480" height="60" controls>
+						<source src="${frappe.utils.escape_html(file_doc.file_url)}" type="audio/mpeg">
+						${__("Your browser does not support the audio element.")}
+					</audio >
+				</div>`);
+			}
 
-		if ($preview && file_preview_field.$wrapper.html() != $preview[0].outerHTML) {
+			if ($preview && file_preview_field.$wrapper.html() != $preview[0].outerHTML) {
+				file_preview_field.$wrapper.html($preview);
+			}
+		} catch(e) {
 			file_preview_field.$wrapper.html($preview);
+			return;
 		}
 	},
 
