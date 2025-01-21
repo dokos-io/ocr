11656dbe8d8bf7956ea8ba45ff416d23aa725cd2
Charles-Henri Decultot <chdecultot@dokos.io>
2025-01-21 10:01:45 +0100

    fix: Override api for v5

--- ocr/overrides/__init__.py
+++ ocr/overrides/__init__.py
@@ -3,11 +3,11 @@ import frappe
 from erpnext.stock.get_item_details import get_item_details as _get_item_details
 
 @frappe.whitelist()
-def get_item_details(args, doc=None, for_validate=False, overwrite_warehouse=True):
-	item_details = _get_item_details(args, doc, for_validate, overwrite_warehouse)
+def get_item_details(ctx, doc=None, for_validate=False, overwrite_warehouse=True):
+	item_details = _get_item_details(ctx, doc, for_validate, overwrite_warehouse)
 
 	if (frappe.parse_json(doc) or {}).get("ocr_request"):
-		doc_values = frappe.parse_json(args) or {}
+		doc_values = frappe.parse_json(ctx) or {}
 		if doc_values:
 			item_details["price_list_rate"] = doc_values.get("net_rate")
 
