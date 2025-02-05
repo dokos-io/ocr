28d22cc4bc9c18d734a12f28fb25b45dd4bba676
Charles-Henri Decultot <chdecultot@dokos.io>
2025-02-05 17:24:25 +0100

    feat: Option to block the validation of a PI if amount exceeds supplier grand total

--- ocr/ocr/doctype/ocr_settings/ocr_settings.py
+++ ocr/ocr/doctype/ocr_settings/ocr_settings.py
@@ -15,6 +15,7 @@ class OCRSettings(Document):
 
 		aws_textract_key: DF.Data | None
 		aws_textract_secret: DF.Password | None
+		block_if_total_exceeds_pending_pi: DF.Check
 		no_purchase_order: DF.Check
 	# end: auto-generated types
 
