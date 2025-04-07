447c13da2e82df579b75c64e11542b1fb0145030
Charles-Henri Decultot <chdecultot@dokos.io>
2025-04-07 16:55:15 +0200

    fix: do not keep previous values for returns

--- ocr/ocr/doctype/pending_purchase_invoice/pending_purchase_invoice.py
+++ ocr/ocr/doctype/pending_purchase_invoice/pending_purchase_invoice.py
@@ -218,12 +218,16 @@ class PendingPurchaseInvoice(Document):
 
 		doc.set("items", [])
 		for item in self.items:
-			doc_item = get_doc_item(item)
 			ppi_row = frappe.copy_doc(item).as_dict()
-			new_row = doc_item.update(ppi_row)
-			for key, value in REFERENCE_FIELDS.get(item.reference_doctype).items():
-				new_row[value] = item.get(key)
-			doc.append("items", new_row)
+			if self.is_return:
+				doc.append("items", ppi_row)
+			else:
+				doc_item = get_doc_item(item)
+				new_row = doc_item.update(ppi_row)
+				for key, value in REFERENCE_FIELDS.get(item.reference_doctype).items():
+					new_row[value] = item.get(key)
+
+				doc.append("items", new_row)
 
 		doc.is_return = bool(self.is_return)
 		if doc.is_return and self.original_invoice:
