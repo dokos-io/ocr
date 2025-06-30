7328a24a73e3ed066fc8b42c15b69fbb0a39a60e
Charles-Henri Decultot <chdecultot@dokos.io>
2025-06-30 21:55:05 +0200

    test: Add debug statement

--- ocr/tests/test_auto_reconciliation.py
+++ ocr/tests/test_auto_reconciliation.py
@@ -51,6 +51,7 @@ class TestAutoReconciliation(IntegrationTestCase):
 		self.assertEqual(len(pending_purchase_invoice.items), 1)
 		self.assertIn(purchase_receipt.name, [r.reference_docname for r in pending_purchase_invoice.items])
 		pending_purchase_invoice.reload()
+		print("test_auto_reconciliation_01", pending_purchase_invoice.as_dict())
 		self.assertEqual(pending_purchase_invoice.status, "Completed")
 
 
@@ -94,6 +95,7 @@ class TestAutoReconciliation(IntegrationTestCase):
 		settings.save()
 
 		pending_purchase_invoice.reload()
+		print("test_auto_reconciliation_02", pending_purchase_invoice.as_dict())
 		pending_purchase_invoice.save()
 		pending_purchase_invoice.reload()
 		self.assertEqual(pending_purchase_invoice.status, "Completed")
