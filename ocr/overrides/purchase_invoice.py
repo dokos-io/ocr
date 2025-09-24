041fa7aabce5c9ba1719df84012eec640f955abe
Charles-Henri Decultot <chdecultot@dokos.io>
2025-09-24 14:10:44 +0200

    fix: multiple fixes for PR reconciliation

--- ocr/overrides/purchase_invoice.py
+++ ocr/overrides/purchase_invoice.py
@@ -21,16 +21,11 @@ def validate_over_billing(doc, method):
 	for item in doc.items:
 		if item.purchase_receipt:
 			pr = frappe.get_doc("Purchase Receipt", item.purchase_receipt)
-			total_per_ref[item.purchase_receipt] = pr.net_total - (flt(pr.per_billed) * pr.net_total)
+			total_per_ref[item.purchase_receipt] = pr.net_total - (flt(pr.per_billed) / 100.0 * pr.net_total)
 		else:
 			frappe.throw(_("All purchase invoice rows must be linked with a purchase receipt"))
 
 	pending_invoicing_amount = sum(total_per_ref.values())
-
-	min_amount = max(
-		flt(pending_invoicing_amount) - flt(settings.max_difference_amount),
-		flt(pending_invoicing_amount) * (1 - flt(settings.max_difference_percentage_on_net_total) / 100)
-	)
 
 	max_amount = min(
 		flt(pending_invoicing_amount) + flt(settings.max_difference_amount),
@@ -38,8 +33,8 @@ def validate_over_billing(doc, method):
 	)
 
 	precision = frappe.db.get_default("currency_precision")
-	if not (flt(min_amount, precision=precision) <= flt(doc.net_total, precision=precision) <= flt(max_amount, precision=precision)):
-		message = _("This purchase invoice can not be submitted because the net total is higher than {0} or lower than {1}").format(fmt_money(max_amount, currency=doc.currency), fmt_money(min_amount, currency=doc.currency))
+	if flt(doc.net_total, precision=precision) > flt(max_amount, precision=precision): # type: ignore
+		message = _("This purchase invoice can not be submitted because the net total is higher than {0}").format(fmt_money(max_amount, currency=doc.currency))
 
 		if method == "validate":
 			frappe.msgprint(message, alert=True, indicator="red")
