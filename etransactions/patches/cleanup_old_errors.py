266a408370af695bfb1f82ed86db6e68c82378f3
Charles-Henri Decultot <chdecultot@dokos.io>
2026-02-12 21:30:06 +0100

    chore: remove older patches

--- etransactions/patches/cleanup_old_errors.py
+++
@@ -1,7 +0,0 @@
-import frappe
-
-def execute():
-	for request in frappe.get_all("OCR Request", filters={"error": ("is", "set")}, fields=["name", "error"]):
-		if request.error not in ["stale", "no result", "no file"]:
-			doc = frappe.get_doc("OCR Request", request.name)
-			doc.reset_status_and_error()
