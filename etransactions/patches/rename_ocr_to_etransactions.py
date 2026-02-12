9fd636ec924c5c7cf1252193a13317231031e786
Charles-Henri Decultot <chdecultot@dokos.io>
2026-02-12 15:46:36 +0100

    fix: add doctypes to renaming patch

--- etransactions/patches/rename_ocr_to_etransactions.py
+++ etransactions/patches/rename_ocr_to_etransactions.py
@@ -4,3 +4,15 @@ from frappe.model.rename_doc import rename_doc
 def execute():
 	if frappe.db.exists("DocType", "OCR Settings") and not frappe.db.exists("DocType", "eTransactions Settings"):
 		rename_doc("DocType", "OCR Settings", "eTransactions Settings", force=True)
+
+
+	if frappe.db.exists("DocType", "OCR Basket") and not frappe.db.exists("DocType", "eTransactions Settings"):
+		rename_doc("DocType", "OCR Basket", "Supplier Invoices Basket", force=True)
+
+
+	if frappe.db.exists("DocType", "Pending Purchase Invoice") and not frappe.db.exists("DocType", "eTransactions Settings"):
+		rename_doc("DocType", "Pending Purchase Invoice", "Supplier Invoice", force=True)
+
+
+	if frappe.db.exists("DocType", "Pending Purchase Invoice Item") and not frappe.db.exists("DocType", "eTransactions Settings"):
+		rename_doc("DocType", "Pending Purchase Invoice Item", "Supplier Invoice Item", force=True)
