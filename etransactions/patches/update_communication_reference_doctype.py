91cec4aedffeffd3744e6d96f699506782016483
Charles-Henri Decultot <chdecultot@dokos.io>
2026-04-15 18:39:45 +0200

    fix: errors in communication patch

--- etransactions/patches/update_communication_reference_doctype.py
+++ etransactions/patches/update_communication_reference_doctype.py
@@ -10,20 +10,11 @@ def execute():
 	for d in communications:
 		frappe.db.set_value("Communication", d.name, "reference_doctype", "Supplier Invoices Basket", update_modified=False)
 
-	# Update timeline_doctype
-	timeline_communications = frappe.get_all(
-		"Communication",
-		filters={"timeline_doctype": "OCR Purchase Invoice Basket"}
-	)
-
-	for d in timeline_communications:
-		frappe.db.set_value("Communication", d.name, "timeline_doctype", "Supplier Invoices Basket", update_modified=False)
-
 	# Update link_doctype
 	link_communications = frappe.get_all(
-		"Communication",
+		"Communication Link",
 		filters={"link_doctype": "OCR Purchase Invoice Basket"}
 	)
 
 	for d in link_communications:
-		frappe.db.set_value("Communication", d.name, "link_doctype", "Supplier Invoices Basket", update_modified=False)
+		frappe.db.set_value("Communication Link", d.name, "link_doctype", "Supplier Invoices Basket", update_modified=False)
